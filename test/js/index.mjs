b576aefd473f40eaa4f2652d0aefd5d14fdf6808
martin jakubik <martin.jakubik@sap.com>
2023-11-26 18:29:40 +0100

    tidy

--- test/js/index.mjs
+++ test/js/index.mjs
@@ -5,6 +5,6 @@ monifesto.layer();
 monifesto.line();
 monifesto.line({x1: 80, y1:400, x2:520, y2:400});
 monifesto.text();
-monifesto.text('monifesto', {x1: 80, y1:400});
+monifesto.text('background layer', {x1: 80, y1:400});
 const foreground = monifesto.layer({width: 600, height: 600}, {fill: '#add8e6a0'});
-foreground.text('foreground layer');
+foreground.text('foreground layer', {x1: 80, y1: 500});
