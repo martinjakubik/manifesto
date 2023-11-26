1c1ba85e759e50b3336b81151de0a0c18bcd7a6e
martin jakubik <martin.jakubik@sap.com>
2023-11-26 19:13:17 +0100

    tested layer width

--- test/js/index.mjs
+++ test/js/index.mjs
@@ -6,5 +6,5 @@ monifesto.line();
 monifesto.line({x: 80, y:400, width:440, height:0});
 monifesto.text();
 monifesto.text('background layer', {x: 80, y:400});
-const foreground = monifesto.layer({x:0, y: 360, width: 600, height: 160}, {fill: '#00eeeea0'});
+const foreground = monifesto.layer({x:20, y: 360, width: 560, height: 160}, {fill: '#ffea00a0'});
 foreground.text('foreground layer', {x: 80, y: 500});
